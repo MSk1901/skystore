4033320bcdf5f702c4e52c89c8888e9d5b81efb7
Mariya Kuznetsova <kuznetsova19.m@gmail.com>
2024-01-29 14:36:18 +0300

    add controllers for home and contacts pages

---
+++ catalog/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'main/home.html')
+
+
+def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f"""Имя: {name}
+Телефон: {phone}
+Сообщение: {message}""")
+    return render(request, 'main/contacts.html')
