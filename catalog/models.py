b4c93b168a584541222acb431c37536c55b68262
Mariya Kuznetsova <kuznetsova19.m@gmail.com>
2024-02-06 11:07:26 +0300

    fill Product model

--- catalog/models.py
+++ catalog/models.py
@@ -1,14 +1,7 @@
 from django.db import models
 
 
-class Product(models.Model):
-
-    def __str__(self):
-        pass
-
-    class Meta:
-        verbose_name = 'продукт'
-        verbose_name_plural = 'продукты'
+NULLABLE = {'blank': True, 'null': True}
 
 
 class Category(models.Model):
@@ -19,3 +12,20 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'категория'
         verbose_name_plural = 'категории'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='наименование')
+    description = models.TextField(verbose_name='описание', **NULLABLE)
+    image = models.ImageField(upload_to='products/', verbose_name='изображение', **NULLABLE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='категория')
+    price = models.IntegerField(verbose_name='цена')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='дата последнего изменения', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.name}, ${self.price}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
