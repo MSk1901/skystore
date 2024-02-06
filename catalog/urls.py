03ca6261cceb4c3ea2d8bf5b3419261a65313d3c
Mariya Kuznetsova <kuznetsova19.m@gmail.com>
2024-02-06 11:07:54 +0300

    add url for media

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from catalog.views import index, contacts
@@ -7,4 +9,4 @@ app_name = 'catalog'
 urlpatterns = [
     path('', index),
     path('contacts/', contacts),
-]
+] + static(settings)
