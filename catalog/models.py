9976e5d5335127e89795fa469826657eea618804
Mariya Kuznetsova <kuznetsova19.m@gmail.com>
2024-02-06 10:25:20 +0300

    set Product model

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+
+    def __str__(self):
+        pass
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
