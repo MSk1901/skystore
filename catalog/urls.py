9d061e10012122e8479fca927cf41292aea59810
Mariya Kuznetsova <kuznetsova19.m@gmail.com>
2024-02-12 23:10:37 +0300

    add surl for product page

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from catalog.views import index, contacts
+from catalog.views import index, contacts, item
 
 app_name = 'catalog'
 
 urlpatterns = [
     path('', index),
     path('contacts/', contacts),
+    path('products/<int:pk>', item, name='item_url')
 ]
