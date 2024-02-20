ae7741d20742276f35b683d841a2cd8a67d68d5c
Mariya Kuznetsova <kuznetsova19.m@gmail.com>
2024-02-20 20:28:05 +0300

    add ContactView CBV

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from catalog.views import contacts, item, ProductListView
+from catalog.views import ContactView, item, ProductListView
 
 app_name = 'catalog'
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='list'),
-    path('contacts/', contacts),
+    path('contacts/', ContactView.as_view(), name='contact'),
     path('products/<int:pk>', item, name='item_url')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView
+from django.views.generic import ListView, View
 
 from catalog.models import Product
 
@@ -8,15 +8,20 @@ class ProductListView(ListView):
     model = Product
 
 
-def contacts(request):
-    if request.method == 'POST':
+class ContactView(View):
+    template_name = 'catalog/contacts.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
+
+    def post(self, request):
         name = request.POST.get('name')
         phone = request.POST.get('phone')
         message = request.POST.get('message')
         print(f"""Имя: {name}
 Телефон: {phone}
 Сообщение: {message}""")
-    return render(request, 'catalog/contacts.html')
+        return render(request, self.template_name)
 
 
 def item(request, pk):
