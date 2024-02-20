99dbb92cfae6aaa76e8b658ff70625f43f959351
Mariya Kuznetsova <kuznetsova19.m@gmail.com>
2024-02-20 20:46:30 +0300

    add BlogPost model

--- catalog/models.py
+++ catalog/models.py
@@ -31,3 +31,20 @@ class Product(models.Model):
     class Meta:
         verbose_name = 'продукт'
         verbose_name_plural = 'продукты'
+
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=150, verbose_name='название')
+    slug = models.CharField(max_length=150, verbose_name='slug', **NULLABLE)
+    content = models.TextField(verbose_name='содержимое')
+    preview = models.ImageField(upload_to='blog_images/', verbose_name='изображение', **NULLABLE)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='дата создания')
+    is_published = models.BooleanField(default=False, verbose_name='опубликовано')
+    view_count = models.IntegerField(default=0, verbose_name='количество просмотров')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
