2f5df80928b564f3f578155da8b67df586ba9b1a
Mariya Kuznetsova <kuznetsova19.m@gmail.com>
2024-02-20 23:07:53 +0300

    add BlogPostCreateView CBV

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from catalog.views import ContactView, ProductListView, ProductDetailView, BlogPostListView
+from catalog.views import ContactView, ProductListView, ProductDetailView, BlogPostListView, BlogPostCreateView
 
 app_name = 'catalog'
 
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', ProductListView.as_view(), name='list'),
     path('contacts/', ContactView.as_view(), name='contact'),
     path('products/<int:pk>', ProductDetailView.as_view(), name='item_url'),
-    path('blog/', BlogPostListView.as_view(), name='posts')
+    path('blog/', BlogPostListView.as_view(), name='posts'),
+    path('blog/create/', BlogPostCreateView.as_view(), name='create_post')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView, View, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, View, DetailView, CreateView
+from pytils.translit import slugify
 
 from catalog.models import Product, BlogPost
 
@@ -26,6 +28,21 @@ class ContactView(View):
 Телефон: {phone}
 Сообщение: {message}""")
         return render(request, self.template_name)
+
+
+class BlogPostCreateView(CreateView):
+    model = BlogPost
+    fields = ('title', 'content', 'preview',)
+    success_url = reverse_lazy('catalog:posts')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_post = form.save()
+            new_post.slug = slugify(new_post.title)
+            new_post.is_published = True
+            new_post.save()
+
+        return super().form_valid(form)
 
 
 class BlogPostListView(ListView):
