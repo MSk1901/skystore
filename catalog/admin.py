85217dfa501c54b03d6b9ca8634adc1e85a1ec1c
Mariya Kuznetsova <kuznetsova19.m@gmail.com>
2024-02-20 21:37:33 +0300

    add admin for BlogPost

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from catalog.models import Category, Product
+from catalog.models import Category, Product, BlogPost
 
 
 @admin.register(Category)
@@ -13,3 +13,10 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'price', 'category',)
     list_filter = ('category',)
     search_fields = ('name', 'description',)
+
+
+@admin.register(BlogPost)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'created_at', 'is_published',)
+    list_filter = ('title',)
+    search_fields = ('title', 'content',)
