1108f6c6b9ee9b652434d1dec4db2e7ececa8a9f
Mariya Kuznetsova <kuznetsova19.m@gmail.com>
2024-03-05 23:41:58 +0300

    add urls for Product CRUD

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
 
 from catalog.views import ContactView, ProductListView, ProductDetailView, BlogPostListView, BlogPostCreateView, \
-    BlogPostUpdateView, BlogPostDetailView, BlogPostDeleteView
+    BlogPostUpdateView, BlogPostDetailView, BlogPostDeleteView, ProductUpdateView, ProductCreateView, ProductDeleteView
 
 app_name = 'catalog'
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='list'),
     path('contacts/', ContactView.as_view(), name='contact'),
+    path('products/create/', ProductCreateView.as_view(), name='create_item'),
+    path('products/update/<int:pk>', ProductUpdateView.as_view(), name='update_item'),
+    path('products/delete/<int:pk>', ProductDeleteView.as_view(), name='delete_item'),
     path('products/<int:pk>', ProductDetailView.as_view(), name='item_url'),
     path('blog/', BlogPostListView.as_view(), name='posts'),
     path('blog/create/', BlogPostCreateView.as_view(), name='create_post'),
